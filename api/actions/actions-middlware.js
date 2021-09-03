43b9dae1af08881a5b3a1aa5ba8ac07930e4eb45
Andrewschaer <andrewschaer@github.com>
2021-09-03 18:58:08 -0400

    final push

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,3 +1,50 @@
 const Actions = require('./actions-model');
+const Projects = require('../projects/projects-model');
 
-// module.exports = { middlewareFunctions }
+function validateActionId(req, res, next) {
+    const { id } = req.params;
+    Actions.get(id)
+      .then(possibleAction => {
+        if (possibleAction) {
+          req.project = possibleAction;
+          next();
+        } else {
+            next({ message: `Action ${req.method} Request Unsuccessful: An Action with the Provided ID does not exist`, status: 404 });
+        }
+      })
+      .catch(next);
+}
+
+function validateProjectId(req, res, next) {
+    const id = req.body.project_id;
+    Projects.get(id)
+      .then(possibleProject => {
+        if (possibleProject) {
+          req.project = possibleProject;
+          next();
+        } else {
+            next({ message: `Action ${req.method} Request Unsuccessful: A Project with the Provided Project_id does not exist`, status: 404 });
+        }
+      })
+      .catch(next);
+}
+
+function validateAction(req, res, next) {
+    if (
+      !req.body.project_id || 
+      !req.body.description ||
+      !req.body.notes ||
+      typeof req.body.description !== 'string' ||
+      typeof req.body.notes !== 'string' ||
+      !req.body.description.trim() ||
+      !req.body.notes.trim() ||
+      req.body.description.length > 128 ||
+      (req.method === 'PUT' && typeof req.body.completed !== ('boolean'))
+    ) {
+        next({ message: `Action ${req.method} Request Unsuccessful: Action Description (up to 128 char.) & Notes are Required`, status: 400 });
+    } else {
+      next();
+    }
+}
+
+module.exports = { validateActionId, validateProjectId, validateAction }
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,28 +1,67 @@
 const express = require('express');
 const Actions = require('./actions-model');
-// const { } = require('./actions-middlware');
+const { validateActionId, validateProjectId, validateAction } = require('./actions-middlware');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
+    Actions.get()
+        .then(allActions => {
+            res.status(200).json(allActions);
+        })
+        .catch(error => {
+            next(error);
+        });
+});
 
-})
+router.get('/:id', validateActionId, (req, res, next) => {
+    Actions.get(req.params.id)
+        .then(action => {
+            res.status(200).json(action);
+        })
+        .catch(error => {
+            next(error);
+        });
+});
 
-router.get('/:id', (req, res) => {
+router.post('/', validateProjectId, validateAction, (req, res, next) => {
+    Actions.insert(req.body)
+    .then(newAction => {
+        res.status(200).json(newAction);
+    })
+    .catch(error => {
+        next(error);
+    });
+});
 
-})
+router.put('/:id', validateActionId, validateProjectId, validateAction, (req, res, next) => {
+    Actions.update(req.params.id, req.body)
+        .then(updatedAction => {
+            res.status(200).json(updatedAction);
+        })
+        .catch(error => {
+            next(error);
+        });
+});
 
-router.post('/', (req, res) => {
+router.delete('/:id', validateActionId, (req, res, next) => {
+    Actions.remove(req.params.id)
+        .then( () => {
+            res.status(200).json();
+        })
+        .catch(error => {
+            next(error);
+        });
+});
 
-})
-
-router.put('/:id', (req, res) => {
-
-})
-
-router.delete('/:id', (req, res) => {
-
-})
-
+router.use((err, req, res, next) => {
+    if(!err.message) {
+        err.message = 'Request failed: An error occurred while processing your request';
+    }
+    console.log(err.message);
+    res.status(err.status || 500).json({
+      message: err.message
+    });
+});
 
 module.exports = router;
